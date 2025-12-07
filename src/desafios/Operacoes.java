898035db9cfdda057b643c5fbcd1438ac9d75fb7
Maria Cláudia <marie@Le-Pomme-de-Marie.local>
2025-12-06 21:15:15 -0300

    Aula 01

---
+++ src/desafios/Operacoes.java
@@ -0,0 +1,11 @@
+package desafios;
+
+public class Operacoes
+{
+    public static void main(String[] args)
+    {
+        int soma = 10 + 5;
+        int subtracao = 10 - 5;
+        System.out.printf("O resultado da soma é %d e o da subtração é %d.", soma, subtracao);
+    }
+}
